3efa30e339abe2933fe5df2859035250bd0e7bf2
gustavorsaul <gustavo.saul@edu.pucrs.br>
2025-05-29 13:56:40 -0300

    redirect endpoint para retornar a url encurtada para a original

---
+++ SOLUCAO.md
@@ -0,0 +1,3 @@
+mongodb compass
+
+docker
--- src/main/java/tech/project/url/shortener/controller/UrlController.java
+++ src/main/java/tech/project/url/shortener/controller/UrlController.java
@@ -8,12 +8,20 @@ import tech.project.url.shortener.controller.dto.ShortenUrlResponse;
 import tech.project.url.shortener.entities.UrlEntity;
 import tech.project.url.shortener.repository.UrlRepository;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
 import java.time.LocalDateTime;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -42,5 +50,21 @@ public class UrlController {
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id") String id) {
+        
+        var url = urlRepository.findById(id);
+
+        if (url.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+        
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+    
     
 }
