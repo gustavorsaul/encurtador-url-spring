7b8f985023e7826c4af698e61a427f86b9aad2cc
gustavorsaul <gustavo.saul@edu.pucrs.br>
2025-05-29 13:44:39 -0300

    shortenUrl endpoint para realizar o encurtamento da url

--- src/main/java/tech/project/url/shortener/controller/UrlController.java
+++ src/main/java/tech/project/url/shortener/controller/UrlController.java
@@ -2,8 +2,15 @@ package tech.project.url.shortener.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import tech.project.url.shortener.controller.dto.ShortenUrlRequest;
+import tech.project.url.shortener.controller.dto.ShortenUrlResponse;
+import tech.project.url.shortener.entities.UrlEntity;
+import tech.project.url.shortener.repository.UrlRepository;
 
+import java.time.LocalDateTime;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +19,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class UrlController {
 
+    private final UrlRepository urlRepository;
+
+    public UrlController(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+
+
     @PostMapping(value = "/shorten-url")
-    public ResponseEntity<Void> shortenUrl(@RequestBody ShortenUrlRequest request) {
+    public ResponseEntity<ShortenUrlResponse> shortenUrl( @RequestBody ShortenUrlRequest request,
+                                            HttpServletRequest servletRequest) {
         
-        return ResponseEntity.ok().build();
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+
+        urlRepository.save(new UrlEntity(id, request.url(), LocalDateTime.now().plusMinutes(1)));
+
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
+
+        return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
     
 }
